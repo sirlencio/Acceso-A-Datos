7c432dbbd6a1caf2f55845c698fba9a8ac708987
sirlencio <carlos.l.p.251001@gmail.com>
2022-09-22 19:14:05 +0200

    Empezado ejercicio 10

---
+++ ficheros binarios/src/diez.java
@@ -0,0 +1,9 @@
+public class diez {
+    //Programa Java que crea un array de elementos de tipo double y lee por teclado el valor de sus elementos.
+    // A continuación escribe el contenido del array en un fichero.
+    // Al principio del fichero se escriben dos enteros con los valores del número de filas y columnas del array
+
+    public static void main(String[] args) {
+
+    }
+}
