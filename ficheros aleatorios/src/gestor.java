7129225c4dd6041a56bb59a8dcd136ce13233f5c
sirlencio <carlos.l.p.251001@gmail.com>
2022-10-06 19:28:21 +0200

    Hecho ejercicios ficheros aleatorios

--- ficheros aleatorios/src/gestor.java
+++ ficheros aleatorios/src/gestor.java
@@ -8,19 +8,18 @@ public class gestor {
     public static void main(String[] args) throws IOException {
 
         Scanner input = new Scanner(System.in);
-        int cursor = 5, id;
+        int cursor = 5, id, salario;
         do {
             menu();
             cursor = input.nextInt();
-            switch
-            (cursor) {
+            switch (cursor) {
                 case 1:
                     System.out.println("Ingrese el id: ");
                     id = input.nextInt();
                     System.out.println("Ingrese el departamento: ");
                     int depart = input.nextInt();
                     System.out.println("Ingrese el salario: ");
-                    int salario = input.nextInt();
+                    salario = input.nextInt();
                     System.out.println("Ingrese el apellido: ");
                     String apellido = input.next();
                     if (!comprobarID(id)) {
@@ -31,6 +30,27 @@ public class gestor {
                     System.out.println("Ingrese el id: ");
                     id = input.nextInt();
                     leer(id);
+                    break;
+                case 3:
+                    System.out.println("Ingrese el id: ");
+                    id = input.nextInt();
+                    System.out.println("Ingrese la suma para el nuevo salario: ");
+                    salario = input.nextInt();
+                    modificar(id, salario);
+                    break;
+                case 4:
+                    System.out.println("Ingrese el id: ");
+                    id = input.nextInt();
+                    borrado(id);
+                    break;
+                case 5:
+                    mostrarBorrados();
+                    break;
+                case 0:
+                    System.out.println("Saliendo");
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
                     break;
             }
 
@@ -108,10 +128,99 @@ public class gestor {
         }
     }
 
+    public static void modificar(int metido, int salar) throws IOException {
+        File fichero = new File("AleatorioEmple.dat");
+        RandomAccessFile fileread = new RandomAccessFile(fichero, "r");
+
+
+        int posicion = (metido - 1) * 32; //para situarnos en el id metido
+        int id, dep, salario = 0;
+        char apellido[] = new char[10], aux;
+        String apellidos = "";
+
+        try {
+            fileread.seek(posicion); //nos posicionamos en posicion
+            id = fileread.readInt(); // obtengo id de empleado
+            if (metido != id) {
+                throw new EOFException("El id no existe");
+            } else {
+                for (int i = 0; i < 10; i++) {
+                    aux = fileread.readChar();
+                    apellido[i] = aux;
+                }
+                apellidos = new String(apellido);
+                dep = fileread.readInt(); //obtengo dep
+                salario = fileread.readInt(); //obtengo salario
+
+                fileread.close(); //cerrar fichero
+
+                posicion = posicion + 4 + 20 + 4; // Se posicion en salario
+                salar = salario + salar;
+
+                RandomAccessFile filewrite = new RandomAccessFile(fichero, "rw");
+
+                filewrite.seek(posicion);
+                filewrite.writeInt(salar);
+                filewrite.close();
+
+                System.out.printf("Apellido: %s,  Salario antiguo: %s, Salario nuevo: %s %n", apellidos, salario, salar);
+            }
+        } catch (EOFException e) {
+            System.out.println("El id no existe");
+        }
+    }
+
+    public static void borrado(int metido) throws IOException {
+        File fichero = new File("AleatorioEmple.dat");
+        RandomAccessFile file = new RandomAccessFile(fichero, "rw");
+
+        int arreglo = 0, posicion = (metido - 1) * 32;
+        int id = -1;
+        file.seek(posicion);
+
+        file.writeInt(id);
+
+        StringBuffer buffer = new StringBuffer(Integer.toString(metido));
+        buffer.setLength(10);
+
+        file.writeChars(buffer.toString());
+        file.writeInt(arreglo);
+        file.writeInt(arreglo);
+        file.close();
+        System.out.println("Empleado borrado");
+    }
+
+    public static void mostrarBorrados() throws IOException {
+        File fichero = new File("AleatorioEmple.dat");
+        RandomAccessFile file = new RandomAccessFile(fichero, "r");
+        int id, dep, posicion = 0;
+        int salario;
+        char apellido[] = new char[10], aux;
+        do { //recorro el fichero
+            file.seek(posicion); //nos posicionamos en posicion
+            id = file.readInt(); // obtengo id de empleado
+            for (int i = 0; i < 10; i++) {
+                aux = file.readChar();
+                apellido[i] = aux;
+            }
+            String apellidos = new String(apellido);
+            dep = file.readInt(); //obtengo dep
+            salario = file.readInt(); //obtengo salario
+            if (id == -1) {
+                System.out.printf("Id empleados borrados: %s %n", apellidos);
+            }
+            posicion = posicion + 32;
+        } while (file.getFilePointer() != file.length());
+        file.close(); //cerrar fichero
+    }
+
     public static void menu() {
         System.out.println("¿Que desea hacer? :");
         System.out.println("1.- Ingresar datos");
         System.out.println("2.- Leer datos");
+        System.out.println("3.- Modificar salario");
+        System.out.println("4.- Borrar empleado");
+        System.out.println("5.- Enseñar ids de empleados borrados");
         System.out.println("0.- Salir");
     }
 }
