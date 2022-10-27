63211a8f166065e4acef79e6de596cfe3dcedd52
sirlencio <carlos.l.p.251001@gmail.com>
2022-10-27 17:54:05 +0200

    Arreglado ejercicio 3 y terminado ejercicio 4

--- Practica Ficheros/src/cuatro.java
+++ Practica Ficheros/src/cuatro.java
@@ -1,8 +1,7 @@
 import java.io.*;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-
+import java.util.HashMap;
 public class cuatro {
 
     //Escribe un programa que lea las últimas cotizaciones que aparecen en la página
@@ -10,56 +9,42 @@ public class cuatro {
     // y las guarde en un HashMap que contenga como clave el nombre de empresa y los datosfecha de hoy,
     // el momento (hh:mm) y el valor de la cotización. Sugerencia hacer lectura con los filtros InputStreamReader y BufferedReader.
 
-    public static void main(String[] args) throws Exception {
-        try {
-            //se abre la conexiòn
-            URL url = new URL("http://www.expansion.com/mercados/cotizaciones/indices/ibex35_I.IB.html?ci%20d=SEM23201");
-            URLConnection conexion = url.openConnection();
-            conexion.connect();
+    public static void main(String[] args) throws IOException {
+        URL url = new URL("https://www.expansion.com/mercados/cotizaciones/indices/ibex35_I.IB.html?ci%20d=SEM23201");
+        URLConnection conexion = url.openConnection();
+        HashMap<String, String> datos = new HashMap<>();
 
-            //Lectura
-            InputStream is = conexion.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            char[] buffer = new char[1000];
-            //ACA EMPIEZA MI CONDICIÓN DESDE ACÁ PODES CAMBIAR VOS
-            int leido;
-            String texto = "";
-            while ((leido = br.read(buffer)) > 0) {
-                String datos = new String(buffer, 0, leido);
-                texto += datos;
-            }
-            String[] tr = texto.split("<tr");
-            String fila = "";
-            for (String linea : tr) {
-                if (linea.contains("Valor")) {
-                    fila = linea;
-                }
-            }
-            String[] td = fila.split("<td");
-            String columna = "";
-            for (String linea : td) {
-                if (linea.contains(",00")) {
-                    System.out.println("---> " + linea);
-                    int pos1 = 0;
-                    int pos2 = 0;
-                    String cotizacion = "";
-                    for (int i = 0; i < linea.length(); i++) {
-                        if (linea.substring(i, i + 1).equals(">")) {
-                            pos1 = i;
-                        } else if (linea.substring(i, i + 1).equals("<")) {
-                            pos2 = i;
-                            break;
-                        }
-                    }
-                    System.out.println("---> " + linea.substring(pos1 + 1, pos2));
-                }
-            }
-            //FIN DE LA CONDICION
+        //Guardamos en guardado el html de la pagina
+        InputStream is = conexion.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
-        } catch (MalformedURLException ex) {
-            System.out.println("Tonto");
-        } catch (IOException ex) {
-            System.out.println("Tonto a");
+        String todo;
+        String guardado = null;
+        while ((todo = br.readLine()) != null)
+            guardado += todo;
+        br.close();
+
+        String tabla = guardado.substring(guardado.indexOf("<table id=\"listado_valores"), guardado.indexOf("</table", guardado.indexOf("<table id=\"listado_valores")));
+        String[] tr = tabla.split("<tr>");
+        String[] td;
+
+        String key = "", value = "";
+        for (int i = 2; i < tr.length; i++) {
+            //System.out.println("Fila " + i + " " + tr[i]);
+            td = tr[i].split("<td([^>]*)>");
+            for (int j = 1; j < td.length; j++) {
+                //System.out.println("Columna " + j + " "+ td[j]);
+                key = td[1].substring(td[1].indexOf(">") + 1, td[1].indexOf("<", td[1].indexOf(">"))); //Guardamos el nombre de la empresa
+                //System.out.println(key);
+                String hora = "Hora: " + td[10].substring(0, td[10].indexOf("<")); //Guardamos la hora de la empresa
+                String valor = "Valor: " + td[2].substring(0, td[2].indexOf("<")); //Guardamos el valor de la empresa
+                value = valor + "\n" + hora;
+                //System.out.println(value);
+            }
+            datos.put(key, value);
+        }
+        for (String i : datos.keySet()) {
+            System.out.println("Nombre: " + i + "\n" + datos.get(i));
         }
     }
 }
